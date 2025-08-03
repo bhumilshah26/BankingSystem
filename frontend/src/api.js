eaec0d34eab9ebb50d8887f27af5601288be695c
bhumilshah26 <bhumil.shah2608@gmail.com>
2025-08-03 22:00:12 +0530

    correct url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'https://bankingsystem-i75o.onrender.com',
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://bankingsystem-i75o.onrender.com',
+  // baseURL: 'http://localhost:5000',
   headers: {
     'Content-Type': 'application/json',
   },
